355ad046e1e95c6a1f3cdd0080b85f9856071e15
rmuraix <35632215+rmuraix@users.noreply.github.com>
2022-11-12 11:47:31 +0900

    Add sheet access

---
+++ src/sheet.ts
@@ -0,0 +1,10 @@
+const _sheetId = PropertiesService.getScriptProperties().getProperty("SHEET_ID") || "";
+const _sheet = SpreadsheetApp.openById(_sheetId).getActiveSheet();
+
+function getCell(row: number, column: number): GoogleAppsScript.Spreadsheet.Range {
+    return _sheet.getRange(row, column);
+}
+
+function setValue(cell: GoogleAppsScript.Spreadsheet.Range, value: string): void {
+    cell.setValue(value);
+}
