a4d8f491146270be2d6566bf90cd46b036671148
rmuraix <35632215+rmuraix@users.noreply.github.com>
2022-11-13 12:08:33 +0900

    Add reminder

---
+++ src/main.ts
@@ -0,0 +1,42 @@
+const _Properties = PropertiesService.getScriptProperties();
+
+function getEnv(name: string): string {
+    return _Properties.getProperty(name) || "";
+}
+
+function doPost(e: any) {
+    const eventObj: any = JSON.parse(e.postData.contents);
+}
+
+// Send reminder to LINE
+function reminder(): void {
+    deleteReminderTrigger();
+
+    // [daysPassed, progressDay, pillColor, message]
+    const v: [string, number, string, string] = createCardVariables();
+
+    const card = createCard(v[0], v[1], v[2], v[3]);
+
+    const token: string = getEnv("TOKEN");
+    const id: string = getEnv("USER_ID");
+
+    postToLine(token, id, "flex", "お薬の時間です", card);
+
+    // Recorded as unrecorded in E1 cell
+    setValue(getCell(1, 5), "1");
+}
+
+function alert() {
+    deleteAlertTrigger();
+
+    const status: number = getCell(1, 5).getValue();
+
+    const token: string = getEnv("TOKEN");
+    const id: string = getEnv("USER_ID");
+
+    let card: object;
+    if (status === 1) {
+        card = createAlertCard();
+        postToLine(token, id, "flex", "⚠飲み忘れていませんか？⚠", card);
+    } else false
+}
---
+++ src/post.ts
@@ -0,0 +1,34 @@
+function postToLine(token: string, userId: string, type: string, alt: string | null, contents: object): void {
+    const headers = {
+        "Content-Type": "application/json; charset=UTF-8",
+        "Authorization": "Bearer " + token,
+    };
+
+    let postData;
+    if (type == "text") {
+        postData = {
+            'to': userId,
+            'messages': [{
+                'type': 'text',
+                'text': contents
+            }]
+        };
+    } else if (type == "flex") {
+        postData = {
+            "to": userId,
+            "messages": [{
+                "type": type,
+                "altText": alt,
+                "contents": contents
+            }]
+        };
+    }
+
+    const options: any = {
+        "method": "post",
+        "headers": headers,
+        "payload": JSON.stringify(postData)
+    };
+
+    UrlFetchApp.fetch("https://api.line.me/v2/bot/message/push", options);
+}
